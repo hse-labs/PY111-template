cea60fea4f8a59c65c0c71387e2b4e59afe4a80b
Kell <maybe.hello.world@gmail.com>
2018-10-18 20:27:07 +0300

    priority queue task added

---
+++ Tasks/priority_queue.py
@@ -0,0 +1,43 @@
+"""
+Priority Queue
+
+Queue priorities are from 0 to 5
+"""
+
+
+def enqueue(elem, priority: int = 0) -> None:
+	"""
+	Operation that add element to the end of the queue
+
+	:param elem: element to be added
+	:return: Nothing
+	"""
+	return None
+
+
+def dequeue():
+	"""
+	Return element from the beginning of the queue
+
+	:return: dequeued element
+	"""
+	return None
+
+
+def peek(ind: int = 0, priority: int = 0):
+	"""
+	Allow you to see at the element in the queue without dequeuing it
+
+	:param ind: index of element (count from the beginning)
+	:return: peeked element
+	"""
+	return None
+
+
+def clear() -> None:
+	"""
+	Clear my queue
+
+	:return: None
+	"""
+	return None
---
+++ Tests/test_priority_queue.py
@@ -0,0 +1,49 @@
+import unittest
+import Tasks.priority_queue as priority_queue
+
+
+class MyTestCase(unittest.TestCase):
+	def setUp(self):
+		priority_queue.clear()
+
+	def test_clear(self):
+		priority_queue.enqueue(3)
+		priority_queue.clear()
+
+		self.assertIsNone(priority_queue.dequeue())
+
+	def test_enqueue_dequeue_without_priority(self):
+		initial_elem = 3
+		priority_queue.enqueue(initial_elem)
+
+		self.assertEqual(initial_elem, priority_queue.dequeue())
+
+	def test_multiple_en_de_queues_without_priority(self):
+		items = [i for i in range(10)]
+
+		for i in items:
+			priority_queue.enqueue(i)
+
+		received_items = []
+		for _ in items:
+			received_items.append(priority_queue.dequeue())
+
+		self.assertEqual(items, received_items)
+
+	def test_peek(self):
+		priority_queue.enqueue(3)
+		priority_queue.enqueue(5)
+
+		self.assertEqual(priority_queue.peek(), 3)
+		self.assertEqual(priority_queue.peek(1), 5)
+		self.assertEqual(priority_queue.peek(), 3)
+
+	def test_en_de_queue_with_priority(self):
+		priority_queue.enqueue(0, 1)
+		priority_queue.enqueue(10, 0)
+
+		self.assertEqual(priority_queue.dequeue(), 10)
+
+
+if __name__ == '__main__':
+	unittest.main()
