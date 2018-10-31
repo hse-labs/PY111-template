ab6b1271ad079ece6fb13351eda85e904b2ff158
Kell <maybe.hello.world@gmail.com>
2018-10-31 21:30:40 +0300

    dijkstra task added

---
+++ Tasks/dijkstra.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+
+def dijkstra_algo(G, starting_node) -> dict:
+	"""
+	Count shortest paths from starting node to all nodes of graph G
+
+	:param G: Graph from NetworkX
+	:param starting_node: starting node from G
+	:return: dict like {'node1': 0, 'node2': 10, '3': 33, ...}, where nodes are nodes from G
+	"""
+
+	return dict()
