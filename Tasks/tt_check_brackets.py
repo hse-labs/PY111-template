4bb050cccae497f40b562d1593f2f983986aee79
Kell <maybe.hello.world@gmail.com>
2019-04-17 17:14:10 +0300

    rm old

--- Tasks/tt_check_brackets.py
+++
@@ -1,9 +0,0 @@
-def check_brackets(brackets_row: str) -> bool:
-	"""
-	Check whether input string is a valid bracket sequence
-
-	:param brackets_row: input string to be checked
-	:return: True if valid, False otherwise
-	"""
-
-	return False
