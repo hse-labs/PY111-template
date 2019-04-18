1addebfb198f6c5f3fe6794c40a536f8e977900e
Alexander Serkin <serkin2005@mail.ru>
2019-04-18 21:21:03 +0300

    Optimised 1 row

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -4,10 +4,23 @@ def check_brackets(brackets_row: str) -> bool:
 	:param brackets_row: input string to be checked
 	:return: True if valid, False otherwise
 	"""
-	brackets = {'(':0, ')':0}
-	for chr in brackets_row:
-		if chr == '(':
-			brackets[chr] += 1
-		elif chr == ')' and brackets[chr] <= brackets['(']:
-			brackets[chr] += 1
+	brackets = {'(': 0, ')': 0}
+	for chr_ in brackets_row:
+		if chr_ == '(' or (chr_ == ')' and brackets[chr_] <= brackets['(']):
+			brackets[chr_] += 1
 	return True if brackets['('] == brackets[')'] else False
+
+
+# def check_brackets(brackets_row: str) -> bool:
+# 	"""
+# 	Check whether input string is a valid bracket sequence
+# 	:param brackets_row: input string to be checked
+# 	:return: True if valid, False otherwise
+# 	"""
+# 	brackets = {'(':0, ')':0}
+# 	for chr in brackets_row:
+# 		if chr == '(':
+# 			brackets[chr] += 1
+# 		elif chr == ')' and brackets[chr] <= brackets['(']:
+# 			brackets[chr] += 1
+# 	return True if brackets['('] == brackets[')'] else False
