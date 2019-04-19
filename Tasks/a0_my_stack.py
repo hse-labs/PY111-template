f1db63b6e8d07ce351cdb6e2a8ac546312ca45dc
Христианов Илья Сергеевич <ILYHRI@avalon.ru>
2019-04-19 21:20:37 +0300

    PEP8 done

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -5,51 +5,52 @@ a = []
 
 
 def push(elem) -> None:
-	"""
-	Operation that add element to stack
+    """
+    Operation that add element to stack
 
-	:param elem: element to be pushed
-	:return: Nothing
-	"""
-	a.append(elem)
-	return None
+    :param elem: element to be pushed
+    :return: Nothing
+    """
+    a.append(elem)
+    return None
 
 
 def pop():
-	"""
-	Pop element from the top of the stack
+    """
+    Pop element from the top of the stack
 
-	:return: popped element
-	"""
-	global a
-	if len(a) == 0:
-		return None
-	else:
-		result = a[-1]
-	del a[-1]
-	return result
+    :return: popped element
+    """
+    global a
+    if len(a) == 0:
+        return None
+    else:
+        result = a[-1]
+    del a[-1]
+    return result
 
 
 def peek(ind: int = 0):
-	"""
-	Allow you to see at the element in the stack without popping it
+    """
+    Allow you to see at the element in the stack without popping it
 
-	:param ind: index of element (count from the top)
-	:return: peeked element
-	"""
-	global a
-	for i in a:
-		if i < 0:
-			return None
-		else:
-			return a[-1-ind]
+    :param ind: index of element (count from the top)
+    :return: peeked element
+    """
+    global a
+    for i in a:
+        if i < 0:
+            return None
+        else:
+            return a[-1-ind]
 
 
 def clear() -> None:
-	"""
-	Clear my stack
+    """
+    Clear my stack
 
-	:return: None
-	"""
-	global a
-	return None
+    :return: None
+    """
+    global a
+    a = []
+    return None
