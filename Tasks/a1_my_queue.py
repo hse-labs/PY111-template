bb3c933e1ef5b76455a30492d00d0bb53df51adf
mariavalieva <movalieva@gmail.com>
2019-04-19 21:07:51 +0300

    fix pep8 in a1

--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -5,48 +5,48 @@ queue = []
 
 
 def enqueue(elem) -> None:
-	"""
-	Operation that add element to the end of the queue
+    """
+    Operation that add element to the end of the queue
 
-	:param elem: element to be added
-	:return: Nothing
-	"""
-	global queue
-	queue.append(elem)
-	return None
+    :param elem: element to be added
+    :return: Nothing
+    """
+    global queue
+    queue.append(elem)
+    return None
 
 
 def dequeue():
-	"""
-	Return element from the beginning of the queue
+    """
+    Return element from the beginning of the queue
 
-	:return: dequeued element
-	"""
-	global queue
-	if queue:
-		elem = queue[0]
-		del queue[0]
-		return elem
-	else:
-		return None
+    :return: dequeued element
+    """
+    global queue
+    if queue:
+        elem = queue[0]
+        del queue[0]
+        return elem
+    else:
+        return None
 
 
 def peek(ind: int = 0):
-	"""
-	Allow you to see at the element in the queue without dequeuing it
+    """
+    Allow you to see at the element in the queue without dequeuing it
 
-	:param ind: index of element (count from the beginning)
-	:return: peeked element
-	"""
-	return queue[ind] if queue[ind] else None
+    :param ind: index of element (count from the beginning)
+    :return: peeked element
+    """
+    return queue[ind] if queue[ind] else None
 
 
 def clear() -> None:
-	"""
-	Clear my queue
+    """
+    Clear my queue
 
-	:return: None
-	"""
-	global queue
-	queue.clear()
-	return None
+    :return: None
+    """
+    global queue
+    queue.clear()
+    return None
