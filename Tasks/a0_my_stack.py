1bd27091366e71421854643acf4336e926beaa31
Жестков Артем Сергеевич <ARTZHE@avalon.ru>
2019-04-19 21:32:55 +0300

    spaces to tabs

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -5,59 +5,59 @@ some_list = []
 
 
 def push(elem) -> None:
-	"""
-	Operation that add element to stack
+    """
+    Operation that add element to stack
 
-	:param elem: element to be pushed
-	:return: Nothing
-	"""
-	global some_list
-	some_list.append(elem)
-	return None
+    :param elem: element to be pushed
+    :return: Nothing
+    """
+    global some_list
+    some_list.append(elem)
+    return None
 
 
 def pop():
-	"""
-	Pop element from the top of the stack
+    """
+    Pop element from the top of the stack
 
-	:return: popped element
-	"""
-	global some_list
-	first_variable = some_list[-1]
-	del some_list[-1]
-	return first_variable
+    :return: popped element
+    """
+    global some_list
+    first_variable = some_list[-1]
+    del some_list[-1]
+    return first_variable
 
 
 def peek(ind: int = 0):
-	"""
-	Allow you to see at the element in the stack without popping it
+    """
+    Allow you to see at the element in the stack without popping it
 
-	:param ind: index of element (count from the top)
-	:return: peeked element
-	"""
-	global some_list
-	try:
-		some_variable = some_list[ind]
-		print(some_variable)
-	except IndexError:
-		print(f"элемента с индексом {ind} не существует")
-	return None
+    :param ind: index of element (count from the top)
+    :return: peeked element
+    """
+    global some_list
+    try:
+        some_variable = some_list[ind]
+        print(some_variable)
+    except IndexError:
+        print(f"элемента с индексом {ind} не существует")
+    return None
 
 
 def clear() -> None:
-	"""
-	Clear my stack
+    """
+    Clear my stack
 
-	:return: None
-	"""
-	global some_list
-	some_list.clear()
-	return None
+    :return: None
+    """
+    global some_list
+    some_list.clear()
+    return None
 
 
 if __name__ == '__main__':
-	push('a')
-	push('b')
-	push('c')
-	pop()
-	peek(1)
+    push('a')
+    push('b')
+    push('c')
+    pop()
+    peek(1)
