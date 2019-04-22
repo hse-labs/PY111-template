3ccc35e09df7eead8688f7b648c8edb783aecf16
Evgeniy <ekostrykin@list.ru>
2019-04-23 00:08:31 +0300

    my_queue commit

--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -3,13 +3,18 @@ My little Queue
 """
 
 
+my_queue = []
+
+
 def enqueue(elem) -> None:
 	"""
+
 	Operation that add element to the end of the queue
 
 	:param elem: element to be added
 	:return: Nothing
 	"""
+	my_queue.append(elem)
 	return None
 
 
@@ -19,23 +24,29 @@ def dequeue():
 
 	:return: dequeued element
 	"""
-	return None
+	if my_queue == []:
+		return None
+	else:
+		return my_queue.pop(0)
 
 
 def peek(ind: int = 0):
 	"""
+
 	Allow you to see at the element in the queue without dequeuing it
 
 	:param ind: index of element (count from the beginning)
 	:return: peeked element
 	"""
-	return None
+	return my_queue[ind]
 
 
 def clear() -> None:
 	"""
+
 	Clear my queue
 
 	:return: None
 	"""
+	my_queue.clear()
 	return None
