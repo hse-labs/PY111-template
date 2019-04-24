e5f24c9ff3036fd6d6a121bce9f6687906a15474
Artem <gr.zhestkov@gmail.com>
2019-04-24 21:18:20 +0300

    b2 done c0 done

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -1,22 +1,40 @@
 """
 Taylor series
 """
+import math
+
 
 def ex(x) -> float:
-	"""
-	Calculate value of e^x with Taylor series
+    """
+    Calculate value of e^x with Taylor series
 
-	:param x: x value
-	:return: e^x value
-	"""
-	return None
+    :param x: x value
+    :return: e^x value
+    """
+    total_series = 1
+
+    for i in range(1, 100):
+        total_series += (x ** i) / math.factorial(i)
+
+    return total_series
 
 
 def sinx(x) -> float:
-	"""
-	Calculate sin(x) with Taylor series
+    """
+    Calculate sin(x) with Taylor series
 
-	:param x: x value
-	:return: sin(x) value
-	"""
-	return None
+    :param x: x value
+    :return: sin(x) value
+    """
+    total_series = x
+
+    for i in range(1, 10):
+        if i % 2 == 1:
+            total_series -= x ** (2 * i + 1) / math.factorial(2 * i + 1)
+        else:
+            total_series += x ** (2 * i + 1) / math.factorial(2 * i + 1)
+    return total_series
+
+
+if __name__ == '__main__':
+    print(ex(5))
--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,7 +5,11 @@ def fib_recursive(n: int) -> int:
 	:param n: number of item
 	:return: Fibonacci number
 	"""
-	return None
+	if n == 1:
+		return 1
+	if n < 1:
+		return 0
+	return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n: int) -> int:
@@ -15,4 +19,15 @@ def fib_iterative(n: int) -> int:
 	:param n: number of item
 	:return: Fibonacci number
 	"""
-	return None
+	a = 0
+	b = 1
+	next_item = 0
+	for i in range(3, n+1):
+		next_item = a + b
+		a = b
+		b = next_item
+	return next_item
+
+
+if __name__ == '__main__':
+	print(fib_iterative(7))
