e88cc133d94cd44258eec08eb3e5c29ffd6f1ec8
Метелев Андрей Александрович <ANDMET@avalon.ru>
2019-04-24 21:12:33 +0300

    ghhlkhlh

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -6,5 +6,22 @@ def binary_search(elem, arr):
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
+	m = 0
+	n = len(arr)
+	if elem < arr[0]:
+		return None
+	while elem != arr[int((m + n) // 2)]:
+		if m + 1 == n:
+			return None
+		elif arr[int((m + n) // 2)] > elem:
+			n = int((m + n) // 2)
+		elif arr[int((m + n) // 2)] < elem:
+			m = int((m + n) // 2)
+		elif elem == arr[int((m + n) // 2)]:
+			return (m + n) // 2
+	return (m + n) // 2
 
-	return None
+
+if __name__ == "__main__":
+	b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+	print(binary_search(13, b))
