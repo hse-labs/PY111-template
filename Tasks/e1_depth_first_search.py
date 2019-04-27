6c51c079e641075a2903fa248b8562a4d4ecea42
mariavalieva <movalieva@gmail.com>
2019-04-27 12:00:03 +0300

    'depth search'

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -2,7 +2,7 @@ from typing import Any
 import networkx as nx
 
 
-def dfs(g: nx.Graph, start_node: Any) -> list:
+def dfs(g: nx.Graph, start_node: Any) -> str:
 	"""
 	Do an depth-first search and returns list of nodes in the visited order
 
@@ -10,5 +10,14 @@ def dfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node of search
 	:return: list of nodes in the visited order
 	"""
+	stack = [start_node]
+	visited = []
+	while stack:
+		i = stack[-1]
+		for j in g[i]:
+			if j not in visited:
+				stack.append(j)
+		visited.append(i)
+		stack.remove(i)
 	print(g, start_node)
-	return list(g.nodes)
+	return ''.join(visited)
