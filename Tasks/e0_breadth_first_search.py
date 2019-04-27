1aa1825cddc73b3e4fbc96af0212956d94ef4b28
Ляушкин Ярослав Игоревич <YARLYA@avalon.ru>
2019-04-27 13:09:42 +0300

    Tasks/.

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Any
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def bfs(g: nx.Graph, start_node: Any) -> list:
@@ -10,5 +11,19 @@ def bfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
+
+	explored_nodes = []	#посещённые вершины
+	stack = [start_node]
+
+	while stack:
+		nb = list(g.neighbors(stack.pop()))
+
+		for i in nb:
+			if i not in explored_nodes:
+				explored_nodes.append(i)
+				stack.append(i)
 	print(g, start_node)
+
 	return list(g.nodes)
+
+
