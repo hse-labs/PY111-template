0903d033609d03e9b05eb015dafb4991b614e6e1
LidiyaU <liduyru@gmail.com>
2019-04-27 13:11:56 +0300

    breadth search resolved

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,6 +1,8 @@
 from typing import Any
 import networkx as nx
+import Tasks.a1_my_queue as q   # вместо append() можно использовать свой модуль queue и
 
+import matplotlib.pyplot as plt
 
 def bfs(g: nx.Graph, start_node: Any) -> list:
 	"""
@@ -10,5 +12,40 @@ def bfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
-	print(g, start_node)
-	return list(g.nodes)
+	visited = [start_node]
+	queue = [start_node]
+
+	while len(queue) != 0:
+		active_node = queue.pop(0)
+		for neighbour in g[active_node]:
+			if neighbour not in visited:
+				visited.append(neighbour)
+				queue.append(neighbour)
+
+
+#g = nx.Graph()
+
+# 	g.add_node("a")
+#
+# 	g.add_nodes_from(["b", "c"])
+# #	G.add_edge('f', 'd')
+# 	g.add_edge('f', 'd', weight=3.7)
+# 	G.add_edges_from([('e', 'r'),('t','q')])
+
+
+	# print(g.nodes())
+	# print(g.edges())
+	# print(list(g.neighbors('e')))
+	# print(g['f'])
+	# nx.draw(g, with_labels=True)
+	# plt.show()
+
+    # print(g, start_node)
+	# return list(g.nodes)
+	print(g.nodes)
+	print(visited)
+	return(visited)
+
+
+
+bfs(nx.Graph([('a','b'), ('a','f'), ('b','g'), ('f', 'g'), ('g','i'), ('g','c'), ('c','h'), ('i','h'), ('h','j'), ('h','d'), ('d','e'), ('g','h'), ('h','e')]),'a')
