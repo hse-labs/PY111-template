6f17cc845e8f401f95327e68ce2c016d2e6dac33
Alexander Serkin <serkin2005@mail.ru>
2019-04-27 13:39:41 +0300

    test passed

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,7 @@
 from typing import Any
 import networkx as nx
+import matplotlib.pyplot as plt
+# import Tasks.a1_my_queue as qu_methods
 
 
 def bfs(g: nx.Graph, start_node: Any) -> list:
@@ -10,5 +12,40 @@ def bfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
-	print(g, start_node)
-	return list(g.nodes)
+	visited = [start_node]
+	queue = [start_node]
+	tracks = {node: [] for node in g.nodes}
+	while queue:
+		element = queue.pop(0)
+		neighbors = list(g.neighbors(element))
+		for node in neighbors:
+			if node not in visited:
+				visited.append(node)
+				queue.append(node)
+				tracks[node].extend((*tracks[element], element))
+	print(tracks)
+	return visited
+
+
+if __name__ == '__main__':
+	graph = nx.Graph()
+	graph.add_nodes_from("ABCDEFGHIJ")
+	graph.add_edges_from([
+		('A', 'B'),
+		('A', 'F'),
+		('B', 'G'),
+		('F', 'G'),
+		('G', 'C'),
+		('G', 'H'),
+		('G', 'I'),
+		('C', 'H'),
+		('I', 'H'),
+		('H', 'D'),
+		('H', 'E'),
+		('H', 'J'),
+		('E', 'D'),
+	])
+	result = bfs(graph, 'A')
+	nx.draw(graph, with_labels=True)
+	plt.show()
+	print(result)
--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,14 +1,49 @@
 from typing import Any
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def dfs(g: nx.Graph, start_node: Any) -> list:
-	"""
-	Do an depth-first search and returns list of nodes in the visited order
+def dfs(g: nx.Graph, start_node: Any) -> str:
+    """
+    Do an depth-first search and returns list of nodes in the visited order
 
-	:param g: input graph
-	:param start_node: starting node of search
-	:return: list of nodes in the visited order
-	"""
-	print(g, start_node)
-	return list(g.nodes)
+    :param g: input graph
+    :param start_node: starting node of search
+    :return: list of nodes in the visited order
+    """
+    visited = []
+    stack = [start_node]
+    tracks = {node: [] for node in g.nodes}
+    while stack:
+        element = stack.pop()
+        visited.append(element)
+        for node in list(g.neighbors(element)):
+            if node not in visited:
+                stack.append(node)
+                tracks[node].extend((*tracks[element], element))
+        # tracks[node].extend((*tracks[element], element))
+        # for node in reversed(neighbors):
+        # 	if node not in visited:
+        # 		visited.append(node)
+        # 		stack.append(node)
+        # 		tracks[node].extend((*tracks[element], element))
+    print(tracks)
+    return "".join(visited)
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from(["ABCDEFG"])
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    result = dfs(graph, 'A')
+    print(result)
+    nx.draw(graph, with_labels=True)
+    plt.show()
+    print(result)
