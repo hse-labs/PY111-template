b16f9fd2261b3b246fe4257601f9edddc08f3da9
mariavalieva <movalieva@gmail.com>
2019-04-27 12:00:31 +0300

    'dijkstra'

--- Tasks/e2_dijkstra.py
+++ Tasks/e2_dijkstra.py
@@ -9,5 +9,14 @@ def dijkstra_algo(g: nx.DiGraph, starting_node: Any) -> dict:
 	:param starting_node: starting node from g
 	:return: dict like {'node1': 0, 'node2': 10, '3': 33, ...} with path costs, where nodes are nodes from g
 	"""
+	paths_cost = {i: float('inf') for i in list(g.nodes)}
+	paths_cost[starting_node] = 0
+	not_visited = list(g.nodes)
+	while not_visited:
+		i = min(not_visited, key=lambda x: paths_cost[x])
+		for j in g[i]:
+			if (paths_cost[i] + g[i][j]['weight']) < paths_cost[j]:
+				paths_cost[j] = paths_cost[i] + g[i][j]['weight']
+		not_visited.remove(i)
 	print(g, starting_node)
-	return dict()
+	return paths_cost
