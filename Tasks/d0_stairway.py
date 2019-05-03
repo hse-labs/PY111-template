2b662bc1cc5deeb6adbfb90166038561ff7dc434
Alexander <33028211+nikotinus@users.noreply.github.com>
2019-05-04 02:37:51 +0300

    Solution found
    Using direct method

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -5,4 +5,9 @@ def stairway_path(stairway: list) -> int:
 	:param stairway: list of ints, where each int is a cost of appropriate step
 	:return: minimal cost of getting to the top
 	"""
-	return None
+	for idx, cell in enumerate(stairway[::-1], 1):
+		if idx == 2:
+			stairway[-idx] += stairway[-idx + 1]
+		elif idx > 2:
+			stairway[-idx] += min(stairway[-idx + 1], stairway[-idx + 2])
+	return min(stair[0], stair[1])
