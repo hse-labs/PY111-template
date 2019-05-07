7093ab4f66e01a2bfb30237aa7af9821f6ae9ef0
Daniil <C:\Users\ \AppData\Roaming\The Bat!>
2019-05-07 13:13:19 +0300

    bfs & dfs

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Any
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def bfs(g: nx.Graph, start_node: Any) -> list:
@@ -10,5 +11,39 @@ def bfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
-	print(g, start_node)
-	return list(g.nodes)
+	visited, queue = [], [start_node]
+	while queue:
+		vertex = queue.pop(0)
+		if vertex not in visited:
+			visited.append(vertex)
+			queue.extend(list(G[vertex]))
+
+	return visited
+
+G = nx.Graph()
+# nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+# edges = [('A', 'B'), ('A', 'F'), ('B', 'G'), ('F', 'G'), ('G', 'I'), ('G', 'H'), ('G', 'C'), ('H', 'D'), ('H', 'J'),
+# 		 ('H', 'D'), ('H', 'E'), ("H", "C"), ("H", "I"), ("D", "E")]
+# G.add_node("ABSCDEGHIJ")
+# edges = [('A', 'C'), ('A', 'B'), ('C', 'F'), ('B', 'D'), ('B', 'E'), ('E', 'F') ]
+G.add_nodes_from("ABCDEFGHIJ")
+G.add_edges_from([
+			('A', 'B'),
+			('A', 'F'),
+			('B', 'G'),
+			('F', 'G'),
+			('G', 'C'),
+			('G', 'H'),
+			('G', 'I'),
+			('C', 'H'),
+			('I', 'H'),
+			('H', 'D'),
+			('H', 'E'),
+			('H', 'J'),
+			('E', 'D'),
+		])
+
+# nx.draw(G, with_labels=True)
+# plt.show()
+
+print(bfs(G, 'A'))
--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,31 @@ def dfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node of search
 	:return: list of nodes in the visited order
 	"""
-	print(g, start_node)
-	return list(g.nodes)
+	visited, stack = [], [start_node]
+	while stack:
+		vertex = stack.pop()
+		if vertex not in visited:
+			visited.append(vertex)
+			stack.extend(g[vertex] )
+
+	return visited
+
+G = nx.Graph()
+G.add_nodes_from("ABCDEFGHIJ")
+G.add_edges_from([
+			('A', 'B'),
+			('A', 'F'),
+			('B', 'G'),
+			('F', 'G'),
+			('G', 'C'),
+			('G', 'H'),
+			('G', 'I'),
+			('C', 'H'),
+			('I', 'H'),
+			('H', 'D'),
+			('H', 'E'),
+			('H', 'J'),
+			('E', 'D'),
+		])
+
+print(dfs(G, 'A'))
