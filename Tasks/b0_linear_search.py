d3e4fe5d392ec75cc58520e0de4bd2fbf4fcf126
Nikita <norunov@stream.ru>
2019-05-15 13:40:25 +0300

    b0_думаю как заказывали ))

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -2,7 +2,6 @@
 This module implements some functions based on linear search algo
 """
 
-
 def min_search(arr) -> int:
     """
     Function that find minimal element in array
@@ -12,10 +11,10 @@ def min_search(arr) -> int:
     """
 
     a = 0
-    b = 0
+    b = arr[a]
     for i in range(len(arr)):
-        # print(arr[i])
-        if arr[i] < a:
-            a = arr[i]
-            b = i
-    return b
+        if arr[i] < b:
+            b = arr[i]
+            a = i
+    return a
+
