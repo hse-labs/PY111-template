cbf1c8fc7715d19eb9b73a8479a71a5e1bb91bb3
VinidiktovEvgenijj <49586805+VinidiktovEvgenijj@users.noreply.github.com>
2019-05-21 18:05:57 +0300

    Add files via upload

---
+++ Tests/e2_test_dijkstra.py
@@ -0,0 +1,60 @@
+import unittest
+import networkx as nx
+from Tasks.e2_dijkstra import dijkstra_algo
+
+
+class MyTestCase(unittest.TestCase):
+	@staticmethod
+	def dict_equal(a: dict, b: dict):
+		for k in a.keys():
+			if a[k] != b[k]:
+				return False
+		return len(a.keys()) == len(b.keys())
+
+	@classmethod
+	def setUpClass(cls):
+		cls.G = nx.DiGraph()
+		cls.G.add_nodes_from([i for i in "ABCDEFG"])
+		cls.G.add_weighted_edges_from([
+			("A", "B", 1),
+			("B", "C", 3),
+			("C", "E", 4),
+			("E", "F", 3),
+			("B", "E", 8),
+			("C", "D", 1),
+			("D", "E", 2),
+			("B", "D", 2),
+			("G", "D", 1),
+			("D", "A", 2),
+		])
+
+	def test_first(self):
+		self.assertTrue(
+			self.dict_equal(
+				dijkstra_algo(self.G, 'A'),
+				{'A': 0, 'B': 1, 'C': 4, 'D': 3, 'E': 5, 'F': 8, 'G': float("inf")}
+			),
+			msg="Результаты неверны. Проверьте реализацию и возвращаемое значение для недостижимых нод (float('inf'))."
+		)
+
+	def test_second(self):
+		self.assertTrue(
+			self.dict_equal(
+				dijkstra_algo(self.G, 'D'),
+				{'A': 2, 'B': 3, 'C': 6, 'D': 0, 'E': 2, 'F': 5, 'G': float('inf')}
+			),
+			msg="Результаты неверны. Проверьте реализацию и возвращаемое значение для недостижимых нод (float('inf'))."
+		)
+
+	def test_third(self):
+		self.assertTrue(
+			self.dict_equal(
+				dijkstra_algo(self.G, 'G'),
+				{'A': 3, 'B': 4, 'C': 7, 'D': 1, 'E': 3, 'F': 6, 'G': 0}
+			),
+			msg="Результаты неверны. Проверьте реализацию и возвращаемое значение для недостижимых нод (float('inf'))."
+		)
+
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ Tests/h0_test_KMP.py
@@ -0,0 +1,30 @@
+import unittest
+from Tasks.h0_KMP import kmp_algo
+
+
+class MyTestCase(unittest.TestCase):
+
+	def test_begin(self):
+		haystack = "Hello, tiny world!"
+		needle = "Hell"
+		self.assertEqual(kmp_algo(haystack, needle), 0, "Right index is 0")
+
+	def test_end(self):
+		haystack = "All these moments will be lost in time..."
+		needle = "time..."
+		self.assertEqual(kmp_algo(haystack, needle), 34, "'time...' is in the end")
+
+	def test_middle(self):
+		haystack = "Шел я лесом, вижу мост, на мосту ворона сохнет. " \
+				   "Взял ее за хвост, положил под мост, пускай ворона мокнет."
+		needle = "Взял"
+		self.assertEqual(kmp_algo(haystack, needle), haystack.index(needle), "Не взяли ворону, ворона вся и высохла.")
+
+	def test_missing(self):
+		haystack = "Because only a ginger can call another ginger ginger!"
+		needle = "afroamerican"
+		self.assertIsNone(kmp_algo(haystack, needle), "Фи, как некультурно, песня о рыжих вообще-то.")
+
+
+if __name__ == '__main__':
+	unittest.main()
