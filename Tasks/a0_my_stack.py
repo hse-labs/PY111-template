516e165ae749c33a44ea5427723c54338a607150
aeksei <aeksei94@gmail.com>
2022-02-04 15:38:12 +0500

    class Based stack

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -4,41 +4,42 @@ My little Stack
 from typing import Any
 
 
-def push(elem: Any) -> None:
-    """
-    Operation that add element to stack
+class Stack:
+    def __init__(self):
+        ...  # todo для основы стека можно использовать список
 
-    :param elem: element to be pushed
-    :return: Nothing
-    """
-    print(elem)
-    return None
+    def push(self, elem: Any) -> None:
+        """
+        Operation that add element to stack
 
+        :param elem: element to be pushed
+        :return: Nothing
+        """
+        print(elem)
+        return None
 
-def pop() -> Any:
-    """
-    Pop element from the top of the stack. If not elements - should return None.
+    def pop(self) -> Any:
+        """
+        Pop element from the top of the stack. If not elements - should return None.
 
-    :return: popped element
-    """
-    return None
+        :return: popped element
+        """
+        return None
 
+    def peek(self, ind: int = 0) -> Any:
+        """
+        Allow you to see at the element in the stack without popping it.
 
-def peek(ind: int = 0) -> Any:
-    """
-    Allow you to see at the element in the stack without popping it.
+        :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
+        :return: peeked element or None if no element in this place
+        """
+        print(ind)
+        return None
 
-    :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-    :return: peeked element or None if no element in this place
-    """
-    print(ind)
-    return None
+    def clear(self) -> None:
+        """
+        Clear my stack
 
-
-def clear() -> None:
-    """
-    Clear my stack
-
-    :return: None
-    """
-    return None
+        :return: None
+        """
+        return None
--- Tests/a0_test_my_stack.py
+++ Tests/a0_test_my_stack.py
@@ -1,48 +1,49 @@
 import unittest
-import Tasks.a0_my_stack as my_stack
+from Tasks import a0_my_stack
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        my_stack.clear()
+        self.my_stack = a0_my_stack.Stack()
+        self.my_stack.clear()
 
     def test_clear(self):
-        my_stack.push(3)
-        my_stack.clear()
+        self.my_stack.push(3)
+        self.my_stack.clear()
 
-        self.assertIsNone(my_stack.pop(), msg="Did you clear the stack?")
+        self.assertIsNone(self.my_stack.pop(), msg="Did you clear the stack?")
 
     def test_empty_stack(self):
-        self.assertIsNone(my_stack.pop(), msg="Should return None if no elements")
+        self.assertIsNone(self.my_stack.pop(), msg="Should return None if no elements")
 
     def test_push_pop(self):
         initial_elem = 3
-        my_stack.push(initial_elem)
+        self.my_stack.push(initial_elem)
 
-        self.assertEqual(initial_elem, my_stack.pop())
+        self.assertEqual(initial_elem, self.my_stack.pop())
 
     def test_multiple_pushes_pops(self):
         items = [i for i in range(10)]
 
         for i in items:
-            my_stack.push(i)
+            self.my_stack.push(i)
 
         received_items = []
         for _ in items:
-            received_items.append(my_stack.pop())
+            received_items.append(self.my_stack.pop())
 
         self.assertEqual(list(reversed(items)), received_items)
 
     def test_peek(self):
-        my_stack.push(7)
-        my_stack.push(3)
-        my_stack.push(5)
+        self.my_stack.push(7)
+        self.my_stack.push(3)
+        self.my_stack.push(5)
 
-        self.assertEqual(5, my_stack.peek())
-        self.assertEqual(3, my_stack.peek(1))
-        self.assertEqual(5, my_stack.peek())
+        self.assertEqual(5, self.my_stack.peek())
+        self.assertEqual(3, self.my_stack.peek(1))
+        self.assertEqual(5, self.my_stack.peek())
 
-        self.assertIsNone(my_stack.peek(100), msg="Should return None if no elements")
+        self.assertIsNone(self.my_stack.peek(100), msg="Should return None if no elements")
 
 
 if __name__ == '__main__':
